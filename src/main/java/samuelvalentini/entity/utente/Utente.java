80e9951f7ad2e6ee383b618e53e693c06d8f1919
Samuel-Valentini <samuelvitalia@gmail.com>
2026-03-27 13:27:09 +0100

    class Utente done

---
+++ src/main/java/samuelvalentini/entity/utente/Utente.java
@@ -0,0 +1,76 @@
+package samuelvalentini.entity.utente;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "utenti")
+public class Utente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id_utente")
+    private UUID idUtente;
+    @Column(name = "nome_utente", nullable = false)
+    private String nomeUtente;
+    @Column(name = "cognome_utente", nullable = false)
+    private String cognomeUtente;
+    @Column(name = "data_di_nascita_utente", nullable = false)
+    private LocalDate dataDiNascitaUtente;
+    @Column(name = "numero_tessera_utente", nullable = false, unique = true, columnDefinition = "BIGSERIAL")
+    private long numeroTesseraUtente;
+
+    public Utente(String nomeUtente, String cognomeUtente, LocalDate dataDiNascitaUtente) {
+        if (nomeUtente == null || nomeUtente.isBlank())
+            throw new IllegalArgumentException("Il nome utente non può essere vuoto");
+        this.nomeUtente = nomeUtente;
+        if (cognomeUtente == null || cognomeUtente.isBlank())
+            throw new IllegalArgumentException("Il cognome utente non può essere vuoto");
+        this.cognomeUtente = cognomeUtente;
+        if (dataDiNascitaUtente.isBefore(dataDiNascitaUtente.minusYears(150)) || dataDiNascitaUtente.isAfter(LocalDate.now()))
+            throw new IllegalArgumentException("Data di nascita errata");
+        this.dataDiNascitaUtente = dataDiNascitaUtente;
+    }
+
+    protected Utente() {
+    }
+
+    public UUID getIdUtente() {
+        return idUtente;
+    }
+
+    public String getNomeUtente() {
+        return nomeUtente;
+    }
+
+    public void setNomeUtente(String nomeUtente) {
+        if (nomeUtente == null || nomeUtente.isBlank())
+            throw new IllegalArgumentException("Il nome utente non può essere vuoto");
+        this.nomeUtente = nomeUtente;
+    }
+
+    public LocalDate getDataDiNascitaUtente() {
+        return dataDiNascitaUtente;
+    }
+
+    public void setDataDiNascitaUtente(LocalDate dataDiNascitaUtente) {
+        if (dataDiNascitaUtente.isBefore(dataDiNascitaUtente.minusYears(150)) || dataDiNascitaUtente.isAfter(LocalDate.now()))
+            throw new IllegalArgumentException("Data di nascita errata");
+        this.dataDiNascitaUtente = dataDiNascitaUtente;
+    }
+
+    public long getNumeroTesseraUtente() {
+        return numeroTesseraUtente;
+    }
+
+    @Override
+    public String toString() {
+        return "Utente{" +
+                "idUtente=" + idUtente +
+                ", nomeUtente='" + nomeUtente + '\'' +
+                ", dataDiNascitaUtente=" + dataDiNascitaUtente +
+                ", numeroTesseraUtente=" + numeroTesseraUtente +
+                '}';
+    }
+}
