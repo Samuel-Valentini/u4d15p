ba72dc3ab64c99f3c1ac6c84fbd15f4c9909fe60
Samuel-Valentini <samuelvitalia@gmail.com>
2026-03-27 14:40:44 +0100

    getByYear done

--- src/main/java/samuelvalentini/Application.java
+++ src/main/java/samuelvalentini/Application.java
@@ -4,7 +4,6 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import samuelvalentini.dao.PubblicazioneDAO;
-import samuelvalentini.entity.pubblicazione.Libro;
 
 public class Application {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("catalogo_bibliografico");
@@ -38,21 +37,60 @@ public class Application {
 //            System.out.println(e.getMessage());
 //        }
 
-        // test salvataggio pubblicazione
 
         PubblicazioneDAO pubblicazioneDAO = new PubblicazioneDAO(entityManager);
+//
+//        // test salvataggio pubblicazione
+//
+//        Libro libro1;
+//        try {
+//            libro1 = new Libro("9788820068066", "Ciao", (short) 2020, 350, "Arturo Sempronio", "Fantasy");
+//            System.out.println("libro creato nell'app");
+//            pubblicazioneDAO.save(libro1);
+//            System.out.println("libro salvato");
+//
+//        } catch (RuntimeException e) {
+//            System.out.println(e.getMessage());
+//        }
 
-        Libro libro1;
-        try {
-            libro1 = new Libro("9788820068066", "Ciao", (short) 2020, 350, "Arturo Sempronio", "Fantasy");
-            System.out.println("libro creato nell'app");
-            pubblicazioneDAO.save(libro1);
-            System.out.println("libro salvato");
+//        //test cancellazione
+//
+//        try {
+//
+//            pubblicazioneDAO.deleteById(UUID.fromString("020ffca7-ee8b-4999-846e-f48672640e19"));
+//
+//        } catch (RuntimeException e) {
+//            System.out.println(e.getMessage());
+//        }
 
-        } catch (RuntimeException e) {
-            System.out.println(e.getMessage());
-        }
+//        //test getByISBN
+//
+//        Libro libro1daServer = (Libro) pubblicazioneDAO.getByISBN("9788820068066");
+//        System.out.println(libro1daServer);
 
+//        //test removeByISBN
+//
+//        pubblicazioneDAO.removeByISBN("9788820068066");
+
+//        //test salvataggio rivista
+//
+//
+//        Rivista rivista1;
+//
+//        try {
+//            rivista1 = new Rivista("8820068060", "Ciao", (short) 2020, 350, PeriodicitaRivista.SEMESTRALE);
+//            System.out.println("rivista creato nell'app");
+//            pubblicazioneDAO.save(rivista1);
+//            System.out.println("rivista salvata");
+//
+//        } catch (RuntimeException e) {
+//            System.out.println(e.getMessage());
+//        }
+
+//        //test cerca per anno
+//
+//        List<Pubblicazione> pubblicazioni2020 = pubblicazioneDAO.getByYear((short) 2020);
+//        System.out.println(pubblicazioni2020);
 
         entityManager.close();
         emf.close();
--- src/main/java/samuelvalentini/dao/PubblicazioneDAO.java
+++ src/main/java/samuelvalentini/dao/PubblicazioneDAO.java
@@ -2,10 +2,12 @@ package samuelvalentini.dao;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.NoResultException;
 import samuelvalentini.entity.pubblicazione.Pubblicazione;
 import samuelvalentini.exceptions.NotFoundException;
 import samuelvalentini.exceptions.NotSavedException;
 
+import java.util.List;
 import java.util.UUID;
 
 public class PubblicazioneDAO {
@@ -43,6 +45,39 @@ public class PubblicazioneDAO {
         entityManager.remove(pubblicazione);
         transaction.commit();
         System.out.println("La pubblicazione " + pubblicazione.getTitolo() + " è stato eliminato con successo!");
+
+    }
+
+    public Pubblicazione getByISBN(String isbn) {
+        try {
+            return entityManager.createQuery("SELECT p FROM Pubblicazione p WHERE p.codiceISBN = :isbn", Pubblicazione.class).setParameter("isbn", isbn).getSingleResult();
+
+        } catch (NoResultException e) {
+            System.out.println(isbn + "non trovato");
+            return null;
+        }
+
+    }
+
+
+    public void removeByISBN(String isbn) {
+        Pubblicazione pubblicazione = this.getByISBN(isbn);
+        EntityTransaction transaction = this.entityManager.getTransaction();
+        transaction.begin();
+        entityManager.remove(pubblicazione);
+        transaction.commit();
+        System.out.println("La pubblicazione " + pubblicazione.getTitolo() + " è stato eliminato con successo!");
+
+    }
+
+    public List<Pubblicazione> getByYear(short anno) {
+        try {
+            return entityManager.createQuery("SELECT p FROM Pubblicazione p WHERE p.annoPubblicazione = :anno", Pubblicazione.class).setParameter("anno", anno).getResultList();
+
+        } catch (NoResultException e) {
+            System.out.println(anno + "nessuna pubblicazione trovata");
+            return null;
+        }
 
     }
 }
